9df9f50b6f55af6f6054aab2f02a7bdb48712e11
us032048 <nichaphat2048@gmail.com>
2026-01-07 15:17:57 +0700

    บันทึกข้อมูลได้ 1 กรณี - close #1

---
+++ entity/b6631376_negative_salary_test.go
@@ -0,0 +1,27 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/asaskevich/govalidator"
+	. "github.com/onsi/gomega"
+)
+
+func TestSalary(t *testing.T) {
+	g := NewGomegaWithT(t)
+
+	t.Run(`Salary must be between 15000 and 200000`, func(t *testing.T) {
+		user := Empolyees {
+			Name:         "nichaphat",
+			Salary:       1000,
+			EmployeeCode: "HR-1024",
+		}
+
+		ok, err := govalidator.ValidateStruct(user)
+
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).NotTo(BeNil())
+
+		g.Expect(err.Error()).To(Equal("Salary must be between 15000 and 200000"))
+	})
+}
